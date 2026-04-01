bd21bfcc7591c5eb7533eb4c995c7c99ff406973
Aarti-kumari-DS <aartikri220207@gmail.com>
2026-04-02 02:24:53 +0530

    Update app.py
    Added three locations

--- static/app.py
+++ static/app.py
@@ -3,3 +3,6 @@ from flask import render_template
 from flask import jsonify
 app = Flask(__name__)
 locations = []
+locations.append({"name":"Main Gate","lat":24.9272,"lon":86.2269})
+locations.append({"name":"Security Room","lat":24.9273,"lon":86.2271})
+locations.append({"name":"Parking Area","lat":24.9273,"lon":86.2263})
