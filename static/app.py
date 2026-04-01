28493777fabffcc51d86186d3235e54aa8cfc16d
Aarti-kumari-DS <aartikri220207@gmail.com>
2026-04-02 02:26:07 +0530

    Update app.py
    added admin related locations

--- static/app.py
+++ static/app.py
@@ -6,3 +6,6 @@ locations = []
 locations.append({"name":"Main Gate","lat":24.9272,"lon":86.2269})
 locations.append({"name":"Security Room","lat":24.9273,"lon":86.2271})
 locations.append({"name":"Parking Area","lat":24.9273,"lon":86.2263})
+locations.append({"name":"Administrative Block","lat":24.9282,"lon":86.2269})
+locations.append({"name":"Principal Office","lat":24.9283,"lon":86.2270})
+locations.append({"name":"Examination Cell","lat":24.9283,"lon":86.2268})
